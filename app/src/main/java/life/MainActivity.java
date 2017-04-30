387bc79c06f9b0fb60526dee1d202fb1ab33d099
life <wswsfe@qq.com>
2017-05-01 00:59:20 +0800

    把自定义步行View改SurfaceView为View

--- app/src/main/java/life/MainActivity.java
+++ app/src/main/java/life/MainActivity.java
@@ -1,14 +1,7 @@
 package life;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,12 +11,11 @@ import life.step.StepSensorBase;
 import life.util.SensorUtil;
 
 public class MainActivity extends AppCompatActivity implements StepSensorBase.StepCallBack, OrientSensor.OrientCallBack {
-    public static final int REQUEST_IMG = 1;
     private final String TAG = "MainActivity";
 
     private TextView mStepText;
     private TextView mOrientText;
-    private MainSurfaceView mMainSurfaceView;
+    private StepView mMainSurfaceView;
 
     private StepSensorBase mStepSensor; // 计步传感器
     private OrientSensor mOrientSensor; // 方向传感器
@@ -34,10 +26,8 @@ public class MainActivity extends AppCompatActivity implements StepSensorBase.St
     public void Step(int stepNum) {
         //  计步回调
         mStepText.setText("步数:" + stepNum);
-
-        // 步长和方向角度转为圆点坐标
-
-        mMainSurfaceView.autoAddPoint(mStepLen, mEndOrient);
+        // 步长和方向角度转为点坐标
+        mMainSurfaceView.autoAddPoint(mStepLen);
     }
 
     @Override
@@ -46,9 +36,8 @@ public class MainActivity extends AppCompatActivity implements StepSensorBase.St
         mOrientText.setText("方向:" + orient);
         mMainSurfaceView.autoDrawArrow(orient);
         // 获取手机转动停止后的方向
-        mEndOrient = SensorUtil.getInstance().getRotateEndOrient(orient);
-
-
+//        mEndOrient = SensorUtil.getInstance().getRotateEndOrient(orient);
+        mEndOrient = orient;
     }
 
     @Override
@@ -59,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements StepSensorBase.St
         setContentView(R.layout.activity_main);
         mStepText = (TextView) findViewById(R.id.step_text);
         mOrientText = (TextView) findViewById(R.id.orient_text);
-        mMainSurfaceView = (MainSurfaceView) findViewById(R.id.step_surfaceView);
+        mMainSurfaceView = (StepView) findViewById(R.id.step_surfaceView);
 
         // 注册计步监听
 //        mStepSensor = new StepSensorPedometer(this, this);
@@ -84,40 +73,5 @@ public class MainActivity extends AppCompatActivity implements StepSensorBase.St
         // 注销传感器监听
         mStepSensor.unregisterStep();
         mOrientSensor.unregisterOrient();
-    }
-
-    public void btnClick(View view) {
-        // 添加背景地图
-        startActivityForResult(new Intent(Intent.ACTION_PICK).setType("image/*"), REQUEST_IMG);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_OK) return;
-
-        // 获取相册图片
-        Cursor cursor = managedQuery(data.getData(), new String[]{MediaStore.Images.Media.DATA}, null, null, null);
-        cursor.moveToFirst();
-        Bitmap bitmap = fitBitmap(cursor.getString(0));
-        mMainSurfaceView.changeBitmap(bitmap);
-    }
-
-    /**
-     * 压缩图片适应屏幕
-     */
-    private Bitmap fitBitmap(String file) {
-        BitmapFactory.Options op = new BitmapFactory.Options();
-
-        // 仅加载图片边界，测量大小
-        op.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(file, op);
-        // 压缩比例
-        op.inSampleSize = Math.max(op.outWidth / getResources().getDisplayMetrics().widthPixels,
-                op.outHeight / getResources().getDisplayMetrics().widthPixels);
-        Log.i(TAG, "压缩比率: " + op.inSampleSize);
-        // 完全加载图片
-        op.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(file, op);
     }
 }
